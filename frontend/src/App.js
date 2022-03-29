241addcfb72e09565ab24932ecfda661a3142309
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-03-30 08:11:55 +0900

    FE // create routes & basic pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
-	return <div className="App">Hello world.</div>;
+	return (
+		<Router>
+			<div className="container">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
