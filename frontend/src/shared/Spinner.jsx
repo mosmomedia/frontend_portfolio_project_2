a5bf5e1301dcb97513c6ddeea854e2c4e7168aeb
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-04-10 03:24:25 +0900

    styling spinner

--- frontend/src/shared/Spinner.jsx
+++ frontend/src/shared/Spinner.jsx
@@ -1,8 +1,18 @@
+import tw from 'twin.macro';
+
+const LoadingContainerStyles = tw.div`
+fixed top-0 right-0 bottom-0 left-0 bg-[rgba(0, 0, 0, 0.2)] z-50 flex justify-center items-center
+`;
+
+const SpinnerStyles = tw.div`
+w-16 h-16 border-8 border-solid border-l-black border-b-transparent  border-r-gray-500 border-t-transparent  rounded-full animate-spin
+`;
+
 function Spinner() {
 	return (
-		<div className="loadingSpinnerContainer">
-			<div className="loadingSpinner"></div>
-		</div>
+		<LoadingContainerStyles>
+			<SpinnerStyles />
+		</LoadingContainerStyles>
 	);
 }
 
--- frontend/src/styles/BaseStyles.js
+++ frontend/src/styles/BaseStyles.js
@@ -29,56 +29,6 @@ p{
 a {
 	${tw` no-underline`}
 }
-
-	.bred {
-		border: 1px solid rgba(255, 0, 0, 0.4);
-	}
-
-	/* .input {
-		box-shadow: rgba(0, 0, 0, 0.11);
-		border: none;
-		background: #fff;
-		color: #000;
-		border-radius: 0.25rem;
-		height: 3rem;
-		width: 100%;
-		outline: none;
-		padding: 0 3rem;
-		font-size: 1rem;
-		margin: 1rem 0;
-	} */
-
-	.loadingSpinnerContainer {
-		position: fixed;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		background-color: rgba(0, 0, 0, 0.2);
-		z-index: 5000;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.loadingSpinner {
-		width: 64px;
-		height: 64px;
-		border: 8px solid;
-		border-color: #000 transparent #555 transparent;
-		border-radius: 50%;
-		animation: spin 1.2s linear infinite;
-	}
-
-	@keyframes spin {
-		0% {
-			transform: rotate(0deg);
-		}
-		100% {
-			transform: rotate(360deg);
-		}
-	}
-
 `;
 
 export default BaseStyles;
